76177bc304309f8088b730b18bebc4f3273cd4ec
Igor Rendulic <igor.rendulic@vecweb.net>
2013-04-16 09:58:25 +0200

    Initial Commit

---
+++ GMaps3D/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GMaps3D
+//
+//  Created by Igor Rendulic on 16/04/2013.
+//  Copyright (c) 2013 Igor Rendulic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GMaps3D/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GMaps3D
+//
+//  Created by Igor Rendulic on 16/04/2013.
+//  Copyright (c) 2013 Igor Rendulic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
